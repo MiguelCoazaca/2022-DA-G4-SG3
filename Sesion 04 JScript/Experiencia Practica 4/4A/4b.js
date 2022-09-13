18ec5d969ef01e35837068628526c5b771c9f603
AlexandraZuñiga <101756523+AleZuHu@users.noreply.github.com>
2022-09-13 01:42:49 -0500

    Delete Sesion 04 JScript/Experiencia Practica 4/4A directory

--- Sesion 04 JScript/Experiencia Practica 4/4A/4b.js
+++
@@ -1,9 +0,0 @@
-function Estudiante (codigo, nombre, apellido)  
-{ 
-    this.codigo = codigo; 
-    this.nombre = nombre; 
-    this.apellido = apellido; 
-}; 
-var estu1 = new Estudiante(2020208042, 'Maria', 'Derbez') 
-console.log(estu1.codigo); 
-console.log(estu1.nombre); 
