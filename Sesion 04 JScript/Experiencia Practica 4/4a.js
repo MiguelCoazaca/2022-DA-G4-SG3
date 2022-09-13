84f72d761ec61c447d002c75ddbd547f0072e46b
AlexandraZuñiga <101756523+AleZuHu@users.noreply.github.com>
2022-09-13 01:42:38 -0500

    Add files via upload

---
+++ Sesion 04 JScript/Experiencia Practica 4/4a.js
@@ -0,0 +1,10 @@
+let persona = 
+{
+    edad: 19,
+    nombre: 'Equipo04',
+    año: 2022,
+    asignatura: 'Desarrollo de Aplicaciones'
+};
+console.log(persona['edad'])
+console.log(persona['nombre'])
+console.log(persona['asignatura'])
