1fc619fb6b7c9aad2e2ff6f8852f8b29e77635e8
NadiaChavez17 <101735211+NadiaChavez17@users.noreply.github.com>
2022-10-04 07:38:35 -0500

    Add files via upload

---
+++ Sesion 06 - REACT/Experiencia Practica 03/ESTILOS/micomponente/micomponente.js
@@ -0,0 +1,23 @@
+
+import styles from './micomponente.module.css';
+
+
+function App(){
+  const objetoEstilo={
+    color:'green',
+    fontSize: '40px'
+  }
+  return (
+    <div>
+      <h1 >PELICULA 1</h1>
+      <h1 >PELICULA 2</h1>
+      <h1>PELICULA 3</h1>
+      <h2>NADIA</h2>
+      <h3>MIGUEL</h3>
+      <h4>ALEXANDRA</h4>
+      
+    </div>
+  );
+}
+
+export default App;
