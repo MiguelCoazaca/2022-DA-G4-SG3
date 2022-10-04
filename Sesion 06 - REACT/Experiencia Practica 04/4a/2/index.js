fd243f9024a5ce84f328a421113bd129737b987d
AlexandraZuñiga <101756523+AleZuHu@users.noreply.github.com>
2022-10-04 06:38:46 -0500

    Add files via upload

--- Sesion 06 - REACT/Experiencia Practica 04/4a/2/index.js
+++ Sesion 06 - REACT/Experiencia Practica 04/4a/2/index.js
@@ -1,1 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+class SegundoConEstado extends React.Component{
+  constructor(props){
+    super(props);
+    //Aquí se puede inicializar cualquier valor
+    this.state ={
+      contador:0,
+      mensaje:""
+    }
+  };
 
+  miSuma = () =>{
+    
+    if(this.state.contador % 2 == 0){
+      console.log(this.state.contador);
+      
+      this.setState({
+        contador : this.state.contador + 1,
+        mensaje : "Falso"        
+      });
+    }
+    else
+      this.setState({
+        contador : this.state.contador + 1,
+        mensaje : "Verdad"        
+      });
+    
+  }
+
+
+  render(){
+    return (
+      <div>
+        <h1 onClick={this.miSuma}>Click para saber si es Verdadero o Falso</h1>
+        <h1>{this.state.contador}</h1>
+        <h1>{this.state.mensaje}</h1>
+      </div>
+    );
+  }
+  
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<SegundoConEstado/>);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
