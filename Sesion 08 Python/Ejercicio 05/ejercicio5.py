38e56394d3d4681628e42e79019d1bc669dbdec1
MiguelCoazaca <101756528+MiguelCoazaca@users.noreply.github.com>
2022-10-25 00:34:02 -0500

    Add files via upload

---
+++ Sesion 08 Python/Ejercicio 05/ejercicio5.py
@@ -0,0 +1,16 @@
+archivo = "Prueba.txt"
+salida = "result.txt"
+busqueda = input("Ingresa búsqueda: ")
+lineas_escribir = []
+with open(archivo, "r") as archivo_lectura:
+    numero_linea = 0
+    for linea in archivo_lectura:
+        numero_linea += 1
+        linea = linea.rstrip()
+        separado = linea.split(" ")
+        if busqueda in separado:
+            lineas_escribir.append(str(numero_linea) + " - " + linea)
+
+with open(salida, "w") as archivo_salida:
+    for linea in lineas_escribir:
+        archivo_salida.write(linea + "\n")
