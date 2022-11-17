6c063e3841f3c76758ffc6748feee44424ff4d45
AlexandraZuñiga <101756523+AleZuHu@users.noreply.github.com>
2022-11-16 23:03:04 -0500

    Add files via upload

--- Sesion 11 - Node JS/Ejercicio 2/ejercicio2.js
+++ Sesion 11 - Node JS/Ejercicio 2/ejercicio2.js
@@ -1,1 +1,23 @@
-
+var mysql = require('mysql');
+var conexion = mysql.createConnection({
+    host:'localhost',
+    port:'3306',
+    database: 'rock',
+    user: 'root',
+    password: '',
+});
+conexion.connect(function(err){
+    if(err){
+        console.log("Error de conexion"+ err.stack);
+        return;
+ }
+ console.log("Esta conectado a: "+conexion.threadId);
+});
+conexion.query('select * from canciones', function(error,results){
+    if(error)
+    throw error;
+    results.forEach(element => {
+        console.log(element);
+    });
+});
+conexion.end();
