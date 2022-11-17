0685d3c3eae62900b2b22850421cae90bd21f63a
AlexandraZuñiga <101756523+AleZuHu@users.noreply.github.com>
2022-11-16 23:03:32 -0500

    Add files via upload

--- Sesion 11 - Node JS/Ejercicio 1/ejercicio1.js
+++ Sesion 11 - Node JS/Ejercicio 1/ejercicio1.js
@@ -1,1 +1,22 @@
 
+var http = require('http');
+
+var server = http.createServer();
+
+function operaciones(petic, resp){
+    resp.writeHead(200,{'content-type':'text/plain'});
+    resp.write('Operaciones con numeros pares \n');
+    resp.write('Suma: 2+4 = 6\n');
+    resp.write('Resta: 4-2 = 2\n');
+    resp.write('Multiplicacion: 4*2 = 8\n');
+    resp.write('Division: 4/2  = 2');
+    
+    resp.end();
+}
+
+
+
+server.on('request',operaciones);
+server.listen(3000,function(){
+    console.log('App funcionando :D en el puerto 3000');
+});
